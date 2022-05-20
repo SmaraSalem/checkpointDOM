1e6fb29c5bbd0b9f39fb15560c2e0a74e5c9234d
salemsmara <salemsmara@gmail.com>
2022-05-20 22:33:21 +0200

    update with forech

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
        /*pour reagir les articles*/
-const elm = document.querySelector(".love");
-elm.addEventListener("click",()=>{    elm.classList.toggle("lovee");});
-
-const elmdeux = document.querySelector(".love1");
-elmdeux.addEventListener("click",()=>elmdeux.classList.toggle("lovee"));
 
 
-const elmtrois = document.querySelector('.love2');
-elmtrois.addEventListener("click",()=>elmtrois.classList.toggle("lovee"));
+       var love=document.querySelectorAll('.love')
+
+love.forEach(el=>{el.addEventListener('click',()=>{el.classList.toggle("lovee"); })
+})
+// const elm = document.querySelector(".love");
+// elm.addEventListener("click",()=>{    elm.classList.toggle("lovee");});
+
+// const elmdeux = document.querySelector(".love1");
+// elmdeux.addEventListener("click",()=>elmdeux.classList.toggle("lovee"));
+
+
+// const elmtrois = document.querySelector('.love2');
+// elmtrois.addEventListener("click",()=>elmtrois.classList.toggle("lovee"));
 
        /*pour supprimer les articles */ 
-const elmsuppun = document.querySelector('.supp1');
-const elmDi = document.getElementById('art1');
-elmsuppun.addEventListener("click",()=>elmDi.remove());
 
-const elmsuppdeux = document.querySelector('.supp2');
-const elmDii = document.getElementById('art2');
-elmsuppdeux.addEventListener("click",()=>elmDii.remove());
 
-const elmsupptrois = document.querySelector('.supp3');
-const elmDiii = document.getElementById('art3');
-elmsupptrois.addEventListener("click",()=>elmDiii.remove());
+       var supp=document.querySelectorAll('.supp')
+supp.forEach(el=>{el.addEventListener('click',()=>{el.parentNode.remove(); })
+})
+//const current = document.querySelector('#main');
+//const parent = current.parentNode;
+// const elmsuppun = document.querySelector('.supp1');
+// const elmDi = document.getElementById('art1');
+// elmsuppun.addEventListener("click",()=>elmDi.remove());
+
+// const elmsuppdeux = document.querySelector('.supp2');
+// const elmDii = document.getElementById('art2');
+// elmsuppdeux.addEventListener("click",()=>elmDii.remove());
+
+// const elmsupptrois = document.querySelector('.supp3');
+// const elmDiii = document.getElementById('art3');
+// elmsupptrois.addEventListener("click",()=>elmDiii.remove());
 
 
 
